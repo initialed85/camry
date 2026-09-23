be3f4090b5a3d1f0f05fe7e92f2c0452923b3609
Edward Beech <initialed85@gmail.com>
2026-09-24 01:10:45 +0800

    WIP on all this

--- build-tag-and-push.sh
+++ build-tag-and-push.sh
@@ -26,8 +26,8 @@ docker build --progress=plain --platform=linux/amd64 -t initialed85/camry-api:la
 docker build --progress=plain --platform=linux/amd64 -t initialed85/camry-segment-producer:latest -f ./docker/segment-producer/Dockerfile .
 docker build --progress=plain --platform=linux/amd64 -t initialed85/camry-stream-producer:latest -f ./docker/stream-producer/Dockerfile .
 docker build --progress=plain --platform=linux/amd64 -t initialed85/camry-frontend:latest -f ./docker/frontend/Dockerfile .
-# note: was pytorch/pytorch:2.4.0-cuda11.8-cudnn9-runtime
 docker build --progress=plain --platform=linux/amd64 -t initialed85/camry-object-detector:latest -f ./docker/object-detector/Dockerfile --build-arg BASE_IMAGE=pytorch/pytorch:2.14.0-cuda13.2-cudnn9-runtime .
+# docker build --progress=plain --platform=linux/amd64 -t initialed85/camry-object-detector:latest -f ./docker/object-detector/Dockerfile.new --build-arg BASE_IMAGE=nvidia/cuda:13.2.1-cudnn-runtime-ubuntu24.04 .
 # docker build --progress=plain --platform=linux/amd64 -t initialed85/camry-object-detector:sm30 -f ./docker/object-detector/Dockerfile --build-arg BASE_IMAGE=dizcza/pytorch-sm30:v1.10.2 .
 # docker build --progress=plain --platform=linux/amd64 -t initialed85/camry-object-detector:amd -f ./docker/object-detector/Dockerfile --build-arg BASE_IMAGE=rocm/pytorch:rocm5.3.2_ubuntu20.04_py3.7_pytorch_1.10.2 .
 # docker build --progress=plain --platform=linux/amd64 -t initialed85/camry-object-detector-v2:latest -f ./docker/object-detector-v2/Dockerfile --build-arg BASE_IMAGE=gocv/opencv:4.12.0-gpu-cuda-11.2.2 .
